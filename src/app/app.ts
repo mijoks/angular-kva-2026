c7b72df942b4a3420f77578548b263dedf5eb696
mijoks <mijokovicsm@gmail.com>
2026-03-02 11:14:45 +0100

    promena

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref, MatButtonModule, MatMenuModule,MatToolbarModule],
+  imports: [
+    RouterOutlet,
+    RouterLinkWithHref,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
+
 }
